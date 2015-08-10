235db14917231abd59f89d0934ac4882f85cb3d2
Peter Chittum <pchittum@salesforce.com>
2015-08-10 15:39:28 +0100

    added resources for query and made get more resilient

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -6,7 +6,7 @@ var equipmentCollName = 'equipment';
 
 /* base warehouse url */
 router.get('/', function(req, res, next) {
-  res.send('yo');
+  res.send({msg:'There is nothing here.'});
 });
 
 //fetch all equipment in that collection
@@ -24,17 +24,78 @@ router.get('/equipment/:id', function(req, res){
   var collection = db.get(equipmentCollName);
 
   var eqId = req.params.id;
-  console.log(eqId);
 
-  //must test with findOne
-  collection.find({"_id":eqId},{},function(e,docs){
-    console.log(docs);
-    res.json(docs);
-  });
+  var queryJSON = {"_id":eqId};
+
+  //check for Id passed in. If not, fail with message.
+  //minimally viable value would have to be 24 chars long...but there
+  //is probably a better way to detect actual valid ObjectIds
+  //probably should also check if there is an API to test for valid Mongo ObjectId's
+  if (eqId.length === 24){
+    collection.find(queryJSON,{},function(e,docs){
+      if (e){
+        var messageTxt = 'There was an error in the MongoDB layer passing the query' + JSON.stringify(queryJSON);
+        res.send({msg:msgTxt});
+      } else {
+        console.log(docs);
+        res.json(docs);
+      }
+    });
+  } else {
+    res.send({msg:'A valid record Id must be used. Use the following: /equipment/<record_id_goes_here>. Valid record Ids conform to MongoDB ObjectId rules of 24 hexadecimal characters.'});
+  }
 });
 
 //add a query REST endpoint where we can search for fields that equal a value
 //for querystring params, use req.query.<paramname>
+router.get('/query', function(req, res){
+  var db = req.db;
+  var objName = req.query.from;
+  var collection = db.get(objName);
+  var queryObj = {};
+  var fieldsObj = {};
+
+  console.log(req.query);
+  console.log(objName);
+
+  //test for object in from param
+  if (!objName){
+    res.json({msg:'The \'from\' query parameter is required and must match a valid object such as \'equipment\''});
+  } else {
+    console.log('we have an object');
+    //unwrap remaining query params.
+    if (req.query.select){
+      fieldsArr = req.query.select.split(',');
+      fieldsArr.forEach(function(item, index, array){
+        fieldsObj[item] = 1;
+      });
+      console.log(fieldsObj.replacement);
+    }
+
+    if (req.query.where){
+      queryArr = req.query.where.split(',');
+      queryArr.forEach(function(item, index, array){
+        var nameValue = item.split('=');
+        var val = nameValue[1];
+        queryObj[nameValue[0]] = (val === 'true' || val === 'false') ?
+                                    Boolean(val) :
+                                    (parseInt(val) === NaN)
+                                      ? val :
+                                      parseInt(val);
+      });
+      console.log(JSON.stringify(queryObj));
+    }
+
+    collection.find(queryObj, fieldsObj, function(err,docs){
+      if (err){
+        res.send({msg:'there was an error retrieving data from the database. Check your query is correctly formed and try again.'});
+      } else {
+        res.json(docs);
+      }
+    });
+  }
+});
+
 
 router.post('/addequipment', function(req, res){
   var db = req.db;
